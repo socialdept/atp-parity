8c784c24b0c584c2ec90aa2031a79628c7bccf2d
Miguel Batres <1373528+btrsco@users.noreply.github.com>
2026-01-10 19:24:44 -0700

    Add PendingSyncManager methods to facade and registry

--- src/Facades/Parity.php
+++ src/Facades/Parity.php
@@ -5,6 +5,7 @@ namespace SocialDept\AtpParity\Facades;
 use Illuminate\Support\Facades\Facade;
 use SocialDept\AtpParity\Contracts\RecordMapper;
 use SocialDept\AtpParity\MapperRegistry;
+use SocialDept\AtpParity\PendingSync\PendingSyncRetryResult;
 
 /**
  * @method static void register(RecordMapper $mapper)
@@ -12,6 +13,9 @@ use SocialDept\AtpParity\MapperRegistry;
  * @method static RecordMapper|null forModel(string $modelClass)
  * @method static array<RecordMapper> forModelAll(string $modelClass)
  * @method static RecordMapper|null forLexicon(string $nsid)
+ * @method static PendingSyncRetryResult retryPendingSyncs(string $did)
+ * @method static bool hasPendingSyncs(string $did)
+ * @method static int countPendingSyncs(string $did)
  *
  * @see MapperRegistry
  */
